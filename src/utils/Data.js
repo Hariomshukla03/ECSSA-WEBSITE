36af8f8e21a8a8fa58efeb346f72f98b0cdfb3b9
HARIOM SHUKLA <hariom09shukla@gmail.com>
2025-09-22 10:04:29 +0530

    changes

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -1,3 +1,2 @@
-export const USERNAME="ECSSA@best";
-export const PASSWORD="ECSSA@president@0987"
-export const BASE_URL="http://localhost:3001"
+
+export const BASE_URL="https://ecssa-backend.onrender.com"
